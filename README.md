edb43690194f09299363a943154acc486b4174cc
zoë <zoe@zoes-mbp.home>
2021-04-05 13:53:29 -0400

    it works, can i use if inside template, no badge

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# ReadMeGenerator
-This application can generate an informative and formatted readMe for any project.
+<# Yert
+<a name="description"></a>
+## Description
+khd;kblkc
+## Table of Contents
+1. [Description] [#description]
+2. [Installation] [#installation]
+3. [Usage] [#usage]
+4  [License] [#license]
+5. [Contributing] [#contributing]
+6. [Tests] [#tests]
+7. [Questions] [#questions]
+
+<a name="installation"></a>
+## Installation
+***
+KHSCLASBD
+
+<a name="usage"></a>
+## Usage
+***
+NDH
+
+<a name="license"></a>
+## License
+***
+Unlicensed
+
+<a name="constributors"></a>
+## Contributing
+***
+undefined
+
+<a name="tests"></a>
+## Tests
+***
+undefined
+
+<a name="questions"></a>
+## Questions
+***
+https://github.com/zchalk
+If you have further questions, suggestions, or business inquiries please contact me via email: mailto:zoechalk1@gmail.com
---
+++ assets/js/index.js
@@ -0,0 +1,115 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const readMeTemplate = (answers) => 
+`<# ${answers.title}
+<a name="description"></a>
+## Description
+${answers.description}
+## Table of Contents
+1. [Description] [#description]
+2. [Installation] [#installation]
+3. [Usage] [#usage]
+4  [License] [#license]
+5. [Contributing] [#contributing]
+6. [Tests] [#tests]
+7. [Questions] [#questions]
+
+<a name="installation"></a>
+## Installation
+***
+${answers.installation}
+
+<a name="usage"></a>
+## Usage
+***
+${answers.usage}
+
+<a name="license"></a>
+## License
+***
+${answers.license}
+
+<a name="constributors></a>
+## Contributing
+***
+${answers.contributing}
+
+<a name="tests"></a>
+## Tests
+***
+${answers.tests}
+
+<a name="questions></a>
+## Questions
+***
+https://github.com/${answers.github}
+If you have further questions, suggestions, or business inquiries please contact me via email: mailto:${answers.email}
+`;
+// TODO: Create an array of questions for user input
+const questions = [
+ {
+     type: 'input',
+     name: 'title',
+     message: 'What is your project called?',
+ },
+ {
+     type: 'input',
+     name: 'description',
+     message: 'What is a good description of your project?',
+ },
+ {
+    type: 'input',
+    name: 'installation',
+    message: 'What should the user know about installation?',
+},
+ {
+     type: 'list',
+     name: 'lisence',
+     choices: ["MIT", "GPLv2", "Apache", "LGPLv3", "none"],
+     message: '',
+ },
+ {
+     type: 'input',
+     name: 'usage',
+     message: 'Is there any important usage information the user should know?',
+ },
+ {
+     type: 'input',
+     name: 'constributing',
+     message: 'Are there any other contibutors, if so list them, if not hit enter?',
+ },
+ {
+     type: 'input',
+     name: 'github',
+     message: 'What is your github username?',
+ },
+ {
+     type: 'input',
+     name: 'email',
+     message: 'What is your email?',
+ },   
+];
+
+// TODO: Create a function to write README file
+const writeToFile = util.promisify(fs.writeFile);
+// function writeToFile(fileName, data) {
+    
+// }
+
+// TODO: Create a function to initialize app
+async function init() {
+    try {
+        const answers = await inquirer.prompt(questions);
+        const template = readMeTemplate(answers);
+        writeToFile('ReadME.md', template);
+    } catch (err) {
+        console.log(err);
+    }
+    
+}
+
+// Function call to initialize app
+init();
